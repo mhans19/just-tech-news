755ec6c7035552d8aa834619627f43564a8503be
mhans19 <mdhansen19@gmail.com>
2020-07-26 15:01:25 -0600

    post routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
 //Remember how in user-routes.js we didn't use the word users in any routes? 
 // That's because in this file we take those routes and implement them to another router instance, 
 // prefixing them with the path /users at that time.
+
+router.use('/posts', postRoutes);
 
 module.exports = router;
 
