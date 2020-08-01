c08ea5acdab801a06c0099be675f9fde7d6275b2
mhans19 <mdhansen19@gmail.com>
2020-07-31 19:15:57 -0600

    added homepage with express-handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+
 router.use('/api', apiRoutes);
 // Much like the API folder's index.js file that collects 
 // the endpoints and prefixes them, here we are collecting 
